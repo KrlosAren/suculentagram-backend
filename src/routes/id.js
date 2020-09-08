04a1a9526f535665f34b97dc2f439ee0adc15d24
Carlos Lopez Z <krlosaren@gmail.com>
2020-09-07 21:54:27 -0300

    fixe id route

--- src/routes/id.js
+++ src/routes/id.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const db = require('../server/db.json');
-const { json } = require('body-parser');
 
 function userIdApi(app) {
-  const router = express.Router();
-  app.use('/users/:id', router);
 
+  app.get('/users/:userId', (req, res) => {
+    return res.send(
+      db.users.find( user => {
+        return user.id === req.params.userId
+      })
+    );
+});
 
-  router.get('/', async (req, res, next) => {
-    const userId = req.params.id
-    const findUser = db.users.find( user => user.id === userId)
-    console.log(userId)
-    res.status(201).send({
-      user: json(findUser)
-    })
-  })
 }
 
 module.exports = userIdApi;
