38d92366679af51c183eb64692a161c37755e4f4
Carlos Lopez Z <krlosaren@gmail.com>
2020-09-22 21:06:05 -0300

    fixe route

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,13 +4,11 @@ const db = require('../server/db.json');
 function postApi(app) {
   app.get('/users/:userId/post/:postId', (req, res) => {
     console.log(req.params)
-    const user = db.users.find( user => user.id === req.params.userId)
-    console.log(user)
-    return res.send(
-      user.posts.find( postUser => {
-        return postUser.id === req.params.postId
-      })
-    );
+    const user = db.users.find(user => user.id === req.params.userId)
+    const post = user.posts.find(postUser => {
+      return postUser.id === req.params.postId
+    })
+    return res.send(post);
   });
 }
 
